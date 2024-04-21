b74ada9fd9b6eba8210fcac7ecf8391a4c7f0db0
Reem <86829326+ReemOthm@users.noreply.github.com>
2024-04-21 16:25:07 +0300

    Update Library.cs

--- src/Library.cs
+++ src/Library.cs
@@ -11,7 +11,7 @@ class Library
     {
         _users = new List<User>();
         _books = new List<Book>();
-        NotificationService = notificationService;
+        this._notificationService = notificationService;
     }
 
     public List<Book> FindBooksByTitle(string title)
@@ -31,11 +31,11 @@ class Library
             bool id = _books.Any(item => item.Id == book.Id);
             if (id)
             {
-                NotificationService.SendNotificationOnFailure($"adding Book '{book.Title}'");
+                _notificationService.SendNotificationOnFailure($"adding Book '{book.Title}', thus it is already exsisting!");
                 return;
             }
             _books.Add(book);
-            NotificationService.SendNotificationOnSucess($"a new book '{book.Title}' has been added");
+            _notificationService.SendNotificationOnSucess($"a new book '{book.Title}' has been added");
 
         }
         catch (Exception e)
@@ -51,11 +51,11 @@ class Library
             bool id = _users.Any(item => item.Id == user.Id);
             if (id)
             {
-                NotificationService.SendNotificationOnFailure($"adding user '{user.Name}', thus it is already existing.");
+                _notificationService.SendNotificationOnFailure($"adding user '{user.Name}', thus it is already existing.");
                 return;
             }
             _users.Add(user);
-            NotificationService.SendNotificationOnSucess($"A new user '{user.Name}' has been added to");
+            _notificationService.SendNotificationOnSucess($"A new user '{user.Name}' has been added to");
 
         }
         catch (Exception e)
@@ -71,11 +71,11 @@ class Library
             bool id = _users.Any(item => item.Id == user.Id);
             if (!id)
             {
-                NotificationService.SendNotificationOnFailure($"deleted user '{user.Name}'");
+                _notificationService.SendNotificationOnFailure($"deleted user '{user.Name}'");
                 return;
             }
             _users.Remove(user);
-            NotificationService.SendNotificationOnSucess($"'{user.Name}' has deleted from");
+            _notificationService.SendNotificationOnSucess($"'{user.Name}' has deleted from");
 
         }
         catch (Exception e)
@@ -91,11 +91,11 @@ class Library
             bool id = _books.Any(item => item.Id == book.Id);
             if (!id)
             {
-                NotificationService.SendNotificationOnFailure($"deleted Book '{book.Title}'");
+                _notificationService.SendNotificationOnFailure($"deleted Book '{book.Title}'");
                 return;
             }
             _books.Remove(book);
-            NotificationService.SendNotificationOnSucess($"Book '{book.Title}' has deleted");
+            _notificationService.SendNotificationOnSucess($"Book '{book.Title}' has deleted");
 
         }
         catch (Exception e)
